919c1e21b91a4986ab155d2ddbb69f41ea78a516
Yasser Bal <yasserbal@icloud.com>
2022-07-26 09:18:13 +0200

    Initial commit Inputhandler contains an object Will create eventlisteners for controls and set it's keys values to true on press, false on release

---
+++ models/InputHandler.js
@@ -0,0 +1,54 @@
+export default class InputHandler {
+  constructor() {
+    this.keys = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    };
+    this.init();
+  }
+  init() {
+    this.addKeyDownEventListeners();
+    this.addKeyUpEventListeners();
+  }
+  addKeyDownEventListeners() {
+    window.addEventListener("keydown", (e) => {
+      console.log(this.keys);
+      //TODO: REFRACTOR KEYS INTO SETTINGS FOR WASD - ARROWKEYS - ZQSD
+      switch (e.key) {
+        case "ArrowUp" || "w" || "z":
+          this.keys.up = true;
+          break;
+        case "ArrowDown" || "s":
+          this.keys.down = true;
+          break;
+        case "ArrowLeft" || "a" || "q":
+          this.keys.left = true;
+          break;
+        case "ArrowRight" || "d":
+          this.keys.right = true;
+          break;
+      }
+    });
+  }
+  addKeyUpEventListeners() {
+    window.addEventListener("keyup", (e) => {
+      //TODO: REFRACTOR KEYS INTO SETTINGS FOR WASD - ARROWKEYS - ZQSD
+      switch (e.key) {
+        case "ArrowUp" || "w" || "z":
+          this.keys.up = false;
+          break;
+        case "ArrowDown" || "s":
+          this.keys.down = false;
+          break;
+        case "ArrowLeft" || "a" || "q":
+          this.keys.left = false;
+          break;
+        case "ArrowRight" || "d":
+          this.keys.right = false;
+          break;
+      }
+    });
+  }
+}
